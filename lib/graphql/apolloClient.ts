ada40494440604da0dbd028eaf341005b809baae
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2022-06-13 11:41:47 -0700

    pull graphql endpoint from env var

--- lib/graphql/apolloClient.ts
+++ lib/graphql/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT || 'https://krypton-etl-staging.herokuapp.com/graphql'
 
 const client = new ApolloClient({
-  uri: 'https://krypton-etl-staging.herokuapp.com/graphql',
+  uri: GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
 })
 
