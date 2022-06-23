398906c7ad8863198f59a0882d97384b14327811
Hakeem Abbas <hakeemsyd@gmail.com>
2022-06-23 21:47:49 +0500

    Hooked Updated Blocks Query
    Hooked Updated Blocks Query And Updated UI

--- components/shared/PaginationButton/index.tsx
+++ components/shared/PaginationButton/index.tsx
@@ -1,49 +1,44 @@
 import * as React from 'react'
 import { CustomArrowButton } from '../SplitButton/styles'
-import { FontStyling } from '../../Blocks/Table/styles'
-import {
-  CustomButtonGroup,
-  ArrowForwardStyle,
-  ArrowBackStyle,
-  CustomButton,
-} from './styles'
+import { CustomButtonGroup, ArrowForwardStyle, ArrowBackStyle } from './styles'
 export interface paginationProps {
   rtl?: string
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>
   currentPage: number
-  totalPages: number | undefined
-  setNext: (endPage: boolean, toNull: boolean) => void
-  setPrevious: (firstPage: boolean, toNull: boolean) => void
+  setNext: () => void
+  setPrevious: () => void
+  lengthOfEachPage: number
+  pageSize: number
 }
 
 export default function PaginationButton({
   rtl,
   setCurrentPage,
   currentPage,
-  totalPages,
   setNext,
   setPrevious,
+  lengthOfEachPage,
+  pageSize,
 }: paginationProps) {
   return (
     <>
       {rtl ? (
         <CustomButtonGroup variant="outlined" disabled={currentPage === 1}>
-          <CustomButton
+          {/* <CustomButton
             onClick={() => {
               setCurrentPage(1)
               setPrevious(true, false)
             }}
           >
             <FontStyling> FIRST </FontStyling>{' '}
-          </CustomButton>
+          </CustomButton> */}
           <CustomArrowButton
             size="small"
             aria-label="select merge strategy"
             aria-haspopup="menu"
             onClick={() => {
               setCurrentPage(currentPage - 1)
-              setPrevious(false, false)
-              setNext(false, true)
+              setPrevious()
             }}
           >
             <ArrowBackStyle disabled={currentPage === 1} />
@@ -52,7 +47,7 @@ export default function PaginationButton({
       ) : (
         <CustomButtonGroup
           variant="outlined"
-          disabled={currentPage === totalPages}
+          disabled={pageSize > lengthOfEachPage}
         >
           <CustomArrowButton
             size="small"
@@ -60,15 +55,14 @@ export default function PaginationButton({
             aria-haspopup="menu"
             onClick={() => {
               setCurrentPage(currentPage + 1)
-              setNext(false, false)
-              setPrevious(false, true)
+              setNext()
             }}
           >
-            <ArrowForwardStyle disabled={currentPage === totalPages} />
+            <ArrowForwardStyle disabled={pageSize > lengthOfEachPage} />
           </CustomArrowButton>
-          <CustomButton>
+          {/* <CustomButton>
             <FontStyling> LAST </FontStyling>{' '}
-          </CustomButton>
+          </CustomButton> */}
         </CustomButtonGroup>
       )}
     </>
