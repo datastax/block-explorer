f9d5ec752c5860142e48e949c08b9088a3b24c86
Hakeem Abbas <hakeemsyd@gmail.com>
2022-12-06 16:40:35 +0500

    Added Console On Verify Token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -2,8 +2,15 @@ import { JWT_KEY } from '@constants'
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import { TokenGenerationPayload } from 'types'
 
-const verifyJWT = (token: string): JwtPayload => {
-  return jwt.verify(token, JWT_KEY) as JwtPayload
+const verifyJWT = (token: string): JwtPayload | undefined => {
+  console.log('token', token)
+  let payload
+  try {
+    payload = jwt.verify(token, JWT_KEY) as JwtPayload
+  } catch (error) {
+    console.log('error==verifyJWT', error)
+  }
+  return payload
 }
 
 const createJWt = (data: TokenGenerationPayload, expiryTime: number) => {
