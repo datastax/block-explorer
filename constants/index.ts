1926196e0155bf8605c00cb5d9da0ef8be91b6f1
Hakeem Abbas <hakeemsyd@gmail.com>
2023-01-02 15:25:47 +0500

    Removed Unused env

--- constants/index.ts
+++ constants/index.ts
@@ -3,13 +3,12 @@ const GRAPHQL_ENDPOINT =
   'https://krypton-etl.herokuapp.com/graphql';
 
 const API_ACCESS_TOKEN = process.env.NEXT_PUBLIC_API_ACCESS_TOKEN;
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
 
 enum PAGINATION_EVENT {
   PREV = 'previous',
   NEXT = 'next',
 }
 
-export { GRAPHQL_ENDPOINT, API_ACCESS_TOKEN, PAGINATION_EVENT, BASE_URL };
+export { GRAPHQL_ENDPOINT, API_ACCESS_TOKEN, PAGINATION_EVENT };
 export * from './routes';
 export * from './stubs';
