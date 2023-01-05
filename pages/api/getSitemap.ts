34e474133c4d7daddb41a5b6003b47b951146bcf
Hakeem Abbas <hakeemsyd@gmail.com>
2023-01-05 23:10:33 +0500

    Fixed Response Size Limit Error

--- pages/api/getSitemap.ts
+++ pages/api/getSitemap.ts
@@ -1,16 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { ISitemapField } from 'next-sitemap';
 import { generateBlockNumberRoutes } from 'utils';
+import zlib from 'zlib';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { startingBlock, latestBlockNumber } = req?.body;
-  const endingBlock = Number(startingBlock) + 100;
-  const data = generateBlockNumberRoutes(
+  const endingBlock = Number(startingBlock) + 50000;
+  let data: ISitemapField[] | unknown = generateBlockNumberRoutes(
     startingBlock,
     endingBlock,
     latestBlockNumber
   );
+
+  const bufferObject = Buffer.from(JSON.stringify(data));
+  const compressedData = await new Promise((resolve, reject) =>
+    zlib.gzip(bufferObject, function (err, zippedData) {
+      if (err) {
+        console.log('Error while generating gzip: ', err);
+        reject(undefined);
+      } else {
+        resolve(zippedData);
+      }
+    })
+  );
+  if (compressedData) data = compressedData;
   res.json({ data });
 }
--- pages/server-sitemaps/[startingBlock].tsx
+++ pages/server-sitemaps/[startingBlock].tsx
@@ -6,6 +6,7 @@ import {
   getLatestBlockGroup,
   getLatestEthBlockNumber,
 } from 'utils';
+import zlib from 'zlib';
 
 export async function getServerSideProps({ res, req, query }: NextPageContext) {
   const { startingBlock } = query;
@@ -20,9 +21,30 @@ export async function getServerSideProps({ res, req, query }: NextPageContext) {
       latestBlockNumber,
     }
   );
-  const urlList = response?.data?.data;
+  let urlList = response?.data?.data;
+  let sitemap;
+  if (urlList?.type === 'Buffer') {
+    const unzippedData = await new Promise<Record<string, unknown>>(
+      (resolve, reject) => {
+        zlib.unzip(Buffer.from(urlList?.data), function (err, unzipped) {
+          const parsedZippedData = JSON.parse(unzipped.toString());
+          if (err) {
+            reject(undefined);
+            console.log('err', err);
+          } else {
+            const mappedSiteMap = createSitemap(parsedZippedData);
+            const decodedUrlList = parsedZippedData;
+            resolve({ mappedSiteMap, decodedUrlList });
+          }
+        });
+      }
+    );
+    if (unzippedData?.mappedSiteMap) sitemap = unzippedData?.mappedSiteMap;
+    if (unzippedData?.decodedUrlList) urlList = unzippedData?.decodedUrlList;
+  } else {
+    sitemap = createSitemap(urlList);
+  }
 
-  const sitemap = createSitemap(urlList);
   res?.setHeader('Content-Type', 'text/xml');
   res?.write(sitemap);
   res?.end();
