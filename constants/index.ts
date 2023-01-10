34b9fa5b4b79c1f8e22605111bcf61564380c287
Hakeem Abbas <hakeemsyd@gmail.com>
2023-01-10 19:28:17 +0500

    Made API Access Token Private

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@ const GRAPHQL_ENDPOINT =
   process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
   'https://krypton-etl.herokuapp.com/graphql';
 
-const API_ACCESS_TOKEN = process.env.NEXT_PUBLIC_API_ACCESS_TOKEN;
+const API_ACCESS_TOKEN = process.env.API_ACCESS_TOKEN;
 
 const SITE_URL = 'https://eth-explorer.datastax.com';
 
